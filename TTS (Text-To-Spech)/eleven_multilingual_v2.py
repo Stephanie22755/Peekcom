1eaca351fdc6e3100bf16b4b05d511267308b331
Stephanie22755 <quochuynguyenle69@gmail.com>
2026-08-17 12:29:00 +0700

    Create eleven_multilingual_v2.py

---
+++ TTS (Text-To-Spech)/eleven_multilingual_v2.py
@@ -0,0 +1,14 @@
+import os
+from elevenlabs.client import ElevenLabs
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.local")
+client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
+response = client.text_to_speech.convert(
+  voice_id="21m00Tcm4TlvDq8ikWAM",
+  model_id="eleven_multilingual_v2",
+  text="This is a test for the API of ElevenLabs."
+)
+with open("output.bin", "wb") as f:
+  f.write(b"".join(response))
+print("Saved output.bin")
